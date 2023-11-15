bf5bd0a9969d188e1dc573b65f3f6ea5529e4807
Munna <munnaislam0172272@gmail.com>
2023-11-15 10:30:13 +0600

    this  code orginal

--- functions.php
+++ functions.php
@@ -3,17 +3,14 @@
 * My Theme Function
 */
 
-function ppr( $log ){
-  echo '<pre>'. print_r( $log ) . '</pre>';
-}
 
 // Theme Title
 add_theme_support('title-tag');
 
 
 // Theme CSS and jQuery File calling
-function ali_css_js_file_calling(){
-  wp_enqueue_style('ali-style', get_stylesheet_uri());
+function ft_css_js_file_calling(){
+  wp_enqueue_style('ft-style', get_stylesheet_uri());
   wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.0.2', 'all');
   wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
   wp_enqueue_style('bootstrap');
@@ -26,51 +23,51 @@ function ali_css_js_file_calling(){
   wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', 'true' );
 
 }
-add_action('wp_enqueue_scripts', 'ali_css_js_file_calling');
+add_action('wp_enqueue_scripts', 'ft_css_js_file_calling');
 
 
 // Google Fonts Enqueue
-function ali_add_google_fonts(){
-  wp_enqueue_style('ali_google_fonts', 'https://fonts.googleapis.com/css2?family=Kaisei+Decol&family=Oswald&display=swap', false);
+function ft_add_google_fonts(){
+  wp_enqueue_style('ft_google_fonts', 'https://fonts.googleapis.com/css2?family=Kaisei+Decol&family=Oswald&display=swap', false);
 }
-add_action('wp_enqueue_scripts', 'ali_add_google_fonts');
+add_action('wp_enqueue_scripts', 'ft_add_google_fonts');
 
 
 //Theme Function
-function ali_customizar_register($wp_customize){
+function ft_customizar_register($wp_customize){
 
   //Header Area Function
-  $wp_customize->add_section('ali_header_area', array(
-    'title' =>__('Header Area', 'alihossain'),
+  $wp_customize->add_section('ft_header_area', array(
+    'title' =>__('Header Area', 'firstthemes'),
     'description' => 'If you interested to update your header area, you can do it here.'
   ));
 
-  $wp_customize->add_setting('ali_logo', array(
+  $wp_customize->add_setting('ft_logo', array(
     'default' => get_bloginfo('template_directory') . '/img/logo.png',
   ));
 
-  $wp_customize-> add_control(new WP_Customize_Image_Control($wp_customize, 'ali_logo', array(
+  $wp_customize-> add_control(new WP_Customize_Image_Control($wp_customize, 'ft_logo', array(
     'label' => 'Logo Upload',
     'description' => 'If you interested to change or update your logo you can do it.',
-    'setting' => 'ali_logo',
-    'section' => 'ali_header_area',
+    'setting' => 'ft_logo',
+    'section' => 'ft_header_area',
   ) ));
 
   // Menu Position Option
-  $wp_customize->add_section('ali_menu_option', array(
-    'title' => __('Menu Position Option', 'alihossain'),
+  $wp_customize->add_section('ft_menu_option', array(
+    'title' => __('Menu Position Option', 'firstthemes'),
     'description' => 'If you interested to change your menu position you can do it.'
   ));
 
-  $wp_customize->add_setting('ali_menu_position', array(
+  $wp_customize->add_setting('ft_menu_position', array(
     'default' => 'right_menu',
   ));
 
-  $wp_customize-> add_control('ali_menu_position', array(
+  $wp_customize-> add_control('ft_menu_position', array(
     'label' => 'Menu Position',
     'description' => 'Select your menu position',
-    'setting' => 'ali_menu_position',
-    'section' => 'ali_menu_option',
+    'setting' => 'ft_menu_position',
+    'section' => 'ft_menu_option',
     'type' => 'radio',
     'choices' => array(
       'left_menu' => 'Left Menu',
@@ -79,27 +76,29 @@ function ali_customizar_register($wp_customize){
     ),
   ));
 
+
+  // Footer Option
+  $wp_customize->add_section('ft_footer_option', array(
+    'title' => __('Footer Option', 'firstthemes'),
+    'description' => 'If you interested to change or update your footer settings you can do it.'
+  ));
+
+  $wp_customize->add_setting('ft_copyright_section', array(
+    'default' => '&copy; Copyright 2021 | Munna BD',
+  ));
+
+  $wp_customize-> add_control('ft_copyright_section', array(
+    'label' => 'Copyright Text',
+    'description' => 'If need you can update your copyright text from here',
+    'setting' => 'ft_copyright_section',
+    'section' => 'ft_footer_option',
+  ));
+
 }
 
-add_action('customize_register', 'ali_customizar_register');
+add_action('customize_register', 'ft_customizar_register');
+
 
 // Menu Register
-register_nav_menu( 'main_menu', __('Main Menu', 'alihossain') );
+register_nav_menu( 'main_menu', __('Main Menu', 'fthossain') );
 
-// walker menu propertis
-function ft_nav_description( $item_output, $item, $arge){
-   if(!empty($item->ft_nav_description)){
-    $item_output = str_replace($args->link_after . '</a>', '<span class="walker_nav">', $item_output);
-   }
-   return $item_output;
-}
-add_filter('walker_nav_menu_start_el', 'ft_nav_description', 10, 3);
-
-
-function ft_nav_description( $item_output, $item, $arge){
-   if(!empty($item->ft_nav_description)){
-    $item_output = str_replace($args->link_after . '</a>', '<span class="walker_nav">', $item_output);
-   }
-   return $item_output;
-}
-add_filter('walker_nav_menu_start_el', 'ft_nav_description', 10, 3);
--- index.php
+++ index.php
@@ -12,11 +12,11 @@
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-  <header id="header_area" class="<?php echo get_theme_mod('ali_menu_position'); ?>">
+  <header id="header_area" class="<?php echo get_theme_mod('ft_menu_position'); ?>">
     <div class="container">
       <div class="row">
         <div class="col-md-3">
-          <a href="<?php echo home_url(); ?>"><img src="<?php echo get_theme_mod('ali_logo'); ?>" alt=""></a>
+          <a href="<?php echo home_url(); ?>"><img src="<?php echo get_theme_mod('ft_logo'); ?>" alt=""></a>
         </div>
         <div class="col-md-9">
           <?php wp_nav_menu( array('theme_location' => 'main_menu', 'menu_id' => 'nav') ); ?>
@@ -34,6 +34,19 @@
       </div>
     </div>
   </section>
+
+  <footer id="footer_area">
+
+    <section id="copyright_area">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <p><?php echo get_theme_mod('ft_copyright_section'); ?></p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </footer>
 
 
   <?php wp_footer(); ?>
