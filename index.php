3e714e26278be5451af81403a39a29ac98fd1f13
Munna <munnaislam0172272@gmail.com>
2023-11-17 15:45:43 +0600

    post code ranning

--- index.php
+++ index.php
@@ -1,54 +1,33 @@
-<?php
-/*
-* This template for displaying the header
-*/
-?>
-<!DOCTYPE html>
-<html lang="<?php language_attributes(); ?>" class="no-js">
-<head>
-  <meta charset="<?php bloginfo('charset') ?>">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <?php wp_head(); ?>
-</head>
-<body <?php body_class(); ?>>
-  <header id="header_area" class="<?php echo get_theme_mod('ft_menu_position'); ?>">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-3">
-          <a href="<?php echo home_url(); ?>"><img src="<?php echo get_theme_mod('ft_logo'); ?>" alt=""></a>
-        </div>
-        <div class="col-md-9">
-          <?php wp_nav_menu( array('theme_location' => 'main_menu', 'menu_id' => 'nav') ); ?>
-        </div>
-      </div>
-    </div>
-  </header>
+<?php 
+/*The main header*/
+ get_header();
+ ?>
 
   <section id="body_area">
     <div class="container">
       <div class="row">
-        <div class="col-md-12">
-          <?php the_content(); ?>
+        <div class="col-md-9">
+          <?php
+            if(have_posts(  )) : 
+              while (have_posts(  )) : the_post();
+          ?>
+
+          <div class="blog_area">
+             <?php the_content(); ?>
+          </div>
+          
+          <?php
+           endwhile;
+          else :
+            -e('no post found');
+          endif;
+          ?>   
+        </div>
+        <div class="col-md-3">
+          <h1>This  is  side ber  area <h1>
         </div>
       </div>
     </div>
   </section>
 
-  <footer id="footer_area">
-
-    <section id="copyright_area">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <p><?php echo get_theme_mod('ft_copyright_section'); ?></p>
-          </div>
-        </div>
-      </div>
-    </section>
-  </footer>
-
-
-  <?php wp_footer(); ?>
-</body>
-</html>
+  <?php get_footer(); ?>
